452a50a22d904fb91d26724f57ac71042bd6b770
Francis Kenne <166624747+harfleur20@users.noreply.github.com>
2026-02-19 14:51:30 +0100

    Create simple-interest.sh for interest calculation
    Add a simple interest calculation script with comments.

---
+++ simple-interest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+   # Ce script calcule l'intérêt simple donné le principal,
+   # le taux d'intérêt annuel et la période de temps en années.
+   # Ne pas utiliser cela en production. À des fins d'exemple uniquement.
+   # Auteur : Upkar Lidder (IBM)
+   # Auteurs supplémentaires :
+   # <harfleur20>
