019c7c34fe3dbbdafcf5a2f4dcf3359a85d3d6c0
Francis Kenne <166624747+harfleur20@users.noreply.github.com>
2026-02-19 14:52:25 +0100

    Modify comments and user prompts in simple-interest.sh
    Updated author information and added input/output comments.

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,4 +4,19 @@
    # Ne pas utiliser cela en production. À des fins d'exemple uniquement.
    # Auteur : Upkar Lidder (IBM)
    # Auteurs supplémentaires :
-   # <harfleur20>
+   # <votre nom d'utilisateur GitHub>
+   # Entrée :
+   # p, montant principal
+   # t, période de temps en années
+   # r, taux d'intérêt annuel
+   # Sortie :
+   # intérêt simple = p*t*r
+   echo "Entrez le principal :"
+   read p
+   echo "Entrez le taux d'intérêt par an :"
+   read r
+   echo "Entrez la période de temps en années :"
+   read t
+   s=`expr $p \* $t \* $r / 100`
+   echo "L'intérêt simple est : "
+   echo $s
